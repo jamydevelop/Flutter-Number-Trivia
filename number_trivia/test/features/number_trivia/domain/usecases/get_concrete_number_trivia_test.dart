ce1e0d72cf3eb8c3973e4ac943f0c79c2489784c
John Ace Magno <127085144+jamydevelop@users.noreply.github.com>
2024-11-29 19:20:32 +0800

    misc files

--- number_trivia/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ number_trivia/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,4 +1,6 @@
+import 'package:dartz/dartz.dart';
 import 'package:mockito/mockito.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.entity.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.usecase.dart';
@@ -15,4 +17,26 @@ void main() {
     usecase =
         GetConcreteNumberTriviaUsecase(repository: mockNumberTriviaRepository);
   });
+
+  const tNumber = 1;
+  const tNumberTrivia = NumberTriviaEntity(number: 1, text: 'test');
+
+  test(
+    'should get trivia for the number from the repository',
+    () async {
+      // "On the fly" implementation of the Repository using the Mockito package.
+      // When getConcreteNumberTrivia is called with any argument, always answer with
+      // the Right "side" of Either containing a test NumberTrivia object.
+      when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
+          .thenAnswer((_) async => const Right(tNumberTrivia));
+      // The "act" phase of the test. Call the not-yet-existent method.
+      final result = await usecase.execute(number: tNumber);
+      // UseCase should simply return whatever was returned from the Repository
+      expect(result, const Right(tNumberTrivia));
+      // Verify that the method has been called on the Repository
+      verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+      // Only the above method should be called and nothing more.
+      verifyNoMoreInteractions(mockNumberTriviaRepository);
+    },
+  );
 }

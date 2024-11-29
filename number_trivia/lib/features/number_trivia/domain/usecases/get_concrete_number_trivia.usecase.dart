8c5478d496b8b19e828d8e2c0622ee721a7297ef
John Ace Magno <127085144+jamydevelop@users.noreply.github.com>
2024-11-29 11:06:20 +0800

    misc files

---
+++ number_trivia/lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.usecase.dart
@@ -0,0 +1,10 @@
+import '../repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTriviaUsecase {
+  final NumberTriviaRepository repository;
+
+  //CONSTRUCTOR
+  GetConcreteNumberTriviaUsecase({
+    required this.repository,
+  });
+}
--- number_trivia/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ number_trivia/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,6 +1,18 @@
 import 'package:mockito/mockito.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.usecase.dart';
 
 class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {}
+
+void main() {
+  GetConcreteNumberTriviaUsecase usecase;
+  MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase =
+        GetConcreteNumberTriviaUsecase(repository: mockNumberTriviaRepository);
+  });
+}
